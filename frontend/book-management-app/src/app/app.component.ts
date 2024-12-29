10f40eaa12dfc754a3bc01d47e607a35219f7b0f
UchithmaSenevirathne <uchithma@gmail.com>
2024-12-29 10:48:52 +0530

    add some styles

--- frontend/book-management-app/src/app/app.component.ts
+++ frontend/book-management-app/src/app/app.component.ts
@@ -7,12 +7,44 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, RouterLink],
   template: `
-    <nav>
-      <a routerLink="/books">Books</a> |
-      <a routerLink="/add-book">Add Book</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+      <div class="container">
+        <a class="navbar-brand" href="#">BookManager</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" routerLink="/books" routerLinkActive="active"
+                >Books</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                routerLink="/add-book"
+                routerLinkActive="active"
+                >Add Book</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-    <router-outlet></router-outlet>
-  `
+
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
+  `,
 })
 export class AppComponent {
   title = 'book-management-app';
